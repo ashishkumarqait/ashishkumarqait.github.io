a78b23ca53afdf59990ad26ee0c81e641ffee5ec
Ashish Kumar <ashish_kumar@qainfotech.com>
2019-09-25 12:18:24 +0530

    sms demo

---
+++ precache-manifest.e457dd5b3a6a6769af08ba8abb055173.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f01cab9e45a117bddfe9c8884c7cea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd1bd0697120cc6e93ad",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f625effb2dde5f1fb2dd",
+    "url": "/static/css/main.3c0eafe2.chunk.css"
+  },
+  {
+    "revision": "bd1bd0697120cc6e93ad",
+    "url": "/static/js/2.0ef92f5e.chunk.js"
+  },
+  {
+    "revision": "f625effb2dde5f1fb2dd",
+    "url": "/static/js/main.8e7f4008.chunk.js"
+  },
+  {
+    "revision": "598a8014c0c32cef60bf",
+    "url": "/static/js/runtime~main.8d5904de.js"
+  },
+  {
+    "revision": "708c2a5d84e7ac3bb30aff8d10b68e37",
+    "url": "/static/media/logo.708c2a5d.png"
+  }
+]);
